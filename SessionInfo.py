26308d230901c62e7557e4221d9b563bcc165de3
YAMMINE Youssef <yoyammine@murex.com>
2024-11-25 16:05:42 +0100

    remove unecessary sys path append from sessionInfo

--- SessionInfo.py
+++ SessionInfo.py
@@ -5,9 +5,6 @@ import sys
 
 import os
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-PARENT_DIR = os.path.dirname(CURRENT_DIR)
-
-sys.path.append(PARENT_DIR)
 
 from p4Helper import P4Helper
 
