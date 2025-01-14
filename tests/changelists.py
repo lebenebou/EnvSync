ac3f1e0c6a1ea406bae4431b652af2bc0626ff21
YAMMINE Youssef <yoyammine@murex.com>
2025-01-14 15:59:37 +0200

    tests: add changelists limit test

---
+++ tests/changelists.py
@@ -0,0 +1,17 @@
+
+import sys
+
+import os
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(CURRENT_DIR)
+
+sys.path.append(PARENT_DIR)
+
+from p4Helper import P4Helper
+import settings
+
+if __name__ == '__main__':
+
+    VERSION = settings.getCurrentVersion()
+
+    assert 2 == len(list(P4Helper.getChangelists(version=VERSION, limit=2))), 'Specifying limit yields wrong number of changelists'
