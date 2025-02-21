52914f71c5dc695a3b7000fc3a629561228e37bb
YAMMINE Youssef <yoyammine@murex.com>
2025-02-21 13:26:55 +0200

    add tests for fetching p4 changelists affected files

--- tests/changelists.py
+++ tests/changelists.py
@@ -7,18 +7,24 @@ PARENT_DIR = os.path.dirname(CURRENT_DIR)
 
 sys.path.append(PARENT_DIR)
 
+from SessionInfo import SessionInfo
 from p4Helper import P4Helper
-import settings
 
 if __name__ == '__main__':
 
-    VERSION = settings.getCurrentVersion()
+    session = SessionInfo()
+    VERSION = session.version
 
     versionCls = list(P4Helper.getChangelists(version=VERSION, limit=2))
-    buildCls = list(P4Helper.getChangelists(version=P4Helper.Build, limit=300))
+    buildCls = list(P4Helper.getChangelists(version=P4Helper.Build, limit=100))
 
     assert 2 == len(versionCls), 'Specifying limit yields wrong number of changelists'
     assert all(not 'mxp4root' in cl.description for cl in buildCls), 'Some changelist xmls were not parsed'
     assert all(int(cl.value) for cl in buildCls), 'Some changelist values are not integers'
 
+    # check if fetching files crashes
+    print(f'Fetching affected files for some changelists on {P4Helper.Build}...', file=sys.stderr)
+    [cl.fetchAffectedFiles() for cl in buildCls]
+
     print('All tests passed')
+    session.close()
