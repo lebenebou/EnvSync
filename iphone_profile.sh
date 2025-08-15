46d18be5e66182b096081d4e131228eb9efbd7a2
lebenebou <lebenebou@gmail.com>
2025-08-15 14:56:37 +0300

    add some aliases

--- iphone_profile.sh
+++ iphone_profile.sh
@@ -7,7 +7,7 @@ cd MiddleMan/
 
 echo "Pulling from git..." >&2 # stderr
 git pull origin main 2>/dev/null
-git reset --hard origin/main 2>/dev/null
+git reset --hard origin/main > /dev/null 2>&1
 
 echo "Updating /root/.profile ..." >&2 # stderr
 [ ! -f "iphone_profile.sh" ] && { echo "iphone_profile.sh does not exist" >&2; exit 1; }
@@ -15,4 +15,9 @@ cat /root/MiddleMan/iphone_profile.sh > /root/.profile
 
 echo "ALL SET" >&2 # stderr
 echo "WELCOME, Youssef" >&2 # stderr
+
+alias ll='ls -l'
+alias back='cd .. && ls'
+alias cls='clear'
+alias reload='source /root/.profile'
 
