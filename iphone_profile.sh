5e397b93f2fccf4675841564121b86fca2f56e0f
lebenebou <lebenebou@gmail.com>
2025-08-15 14:38:11 +0300

    improve iphone_profile.sh

--- iphone_profile.sh
+++ iphone_profile.sh
@@ -1,7 +1,18 @@
 
 clear
-cd MiddleMan/
-git pull origin main
 
-cat .iphone_profile > /root/.profile
+echo "Re-directing into MiddleMan/..." >&2 # stderr
+[ ! -d "MiddleMan" ] && { echo "MiddleMan/ directory does not exist." >&2; exit 1; }
+cd MiddleMan/
+
+echo "Pulling from git..." >&2 # stderr
+git pull origin main
+git reset --hard origin/main
+
+echo "Updating /root/.profile ..." >&2 # stderr
+[ ! -f "iphone_profile.sh" ] && { echo "iphone_profile.sh does not exist" >&2; exit 1; }
+cat /root/MiddleMan/iphone_profile.sh > /root/.profile
+
+echo "ALL SET" >&2 # stderr
+echo "WELCOME, Youssef" >&2 # stderr
 
