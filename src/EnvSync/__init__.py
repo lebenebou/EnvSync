b498733e7ff9970f2246dee349ebd0b419d86e40
lebenebou <lebenebou@gmail.com>
2025-10-26 19:56:06 +0200

    implement encrypt and decrypt functions

--- src/EnvSync/__init__.py
+++ src/EnvSync/__init__.py
@@ -3,6 +3,9 @@ import os
 import sys
 import json
 
+import shutil
+import subprocess
+
 def findBashProfilePath(homeDir: str) -> str:
 
     options = ['.bash_profile', '.bashrc', '.profile']
@@ -12,7 +15,7 @@ def findBashProfilePath(homeDir: str) -> str:
         if os.path.exists(fullPath):
             return fullPath
 
-    print('[WARN]: No bash profile file found in home directory.', file=sys.stderr)
+    print('[WARN] No bash profile file found in home directory.', file=sys.stderr)
     return None
 
 def findVimRcPath(homeDir: str) -> str:
@@ -24,7 +27,7 @@ def findVimRcPath(homeDir: str) -> str:
         if os.path.exists(fullPath):
             return fullPath
 
-    print('[WARN]: No vimrc file found in home directory.', file=sys.stderr)
+    print('[WARN] No vimrc file found in home directory.', file=sys.stderr)
     return None
 
 def readJsonFile(filePath: str) -> dict:
@@ -72,14 +75,37 @@ class GlobalEnv:
             passphrase = input('Enter encryption passphrase: ')
 
         if not passphrase:
-            print('[WARN]: No encryption passphrase found.', file=sys.stderr)
+            print('[WARN] No encryption passphrase found.', file=sys.stderr)
 
         return passphrase
 
     @staticmethod
     def encryptFile(inputFile: str, outputFile: str, passphrase: str):
 
-        assert False, 'Not implemented yet'
+        assert os.path.isfile(inputFile), f'cannot ecnrypt file that does not exist: {inputFile}'
+
+        command = f'openssl enc -aes-256-cbc -pbkdf2 -in {inputFile} -out {outputFile} -pass pass:{passphrase}'
+        result = subprocess.run(command.split())
+
+        if result.returncode != 0:
+            print(f'[ERROR] Failed to encrypt file: {inputFile}', file=sys.stderr)
+            return
+
+        return
+
+    @staticmethod
+    def decryptFile(inputFile: str, outputFile: str, passphrase: str) -> int:
+
+        assert os.path.isfile(inputFile), f'cannot decrypt file that does not exist: {inputFile}'
+
+        command = f'openssl enc -d -aes-256-cbc -pbkdf2 -in {inputFile} -out {outputFile} -pass pass:{passphrase}'
+        result = subprocess.run(command.split())
+
+        if result.returncode != 0:
+            print(f'[ERROR] Failed to decrypt file: {inputFile}', file=sys.stderr)
+            return result.returncode
+        
+        return 0
 
     @staticmethod
     def encryptFiles(deleteDecryptedWhenDone: bool = False, cmdFallback: bool = False):
@@ -108,5 +134,41 @@ class GlobalEnv:
         return
 
     @staticmethod
-    def decryptFiles(cmdFallback: bool = False):
-        pass
+    def decryptFiles(overwriteDecryptedDir: bool = False, cmdFallback: bool = False):
+        
+        if not overwriteDecryptedDir and os.path.exists(GlobalEnv.DECRYPTED_PATH):
+            print(f'[WARN] Decrypted directory already exists: {GlobalEnv.DECRYPTED_PATH}', file=sys.stderr)
+            print(f'[WARN] NO DECRYPTION was done.', file=sys.stderr)
+            return
+
+        if overwriteDecryptedDir and os.path.exists(GlobalEnv.DECRYPTED_PATH):
+            shutil.rmtree(GlobalEnv.DECRYPTED_PATH, ignore_errors=False)
+
+        passphrase: str = GlobalEnv.getEcryptionPassphrase(cmdFallback)
+
+        for root, dirs, files in os.walk(GlobalEnv.ENCRYPTED_PATH):
+
+            encryptedFiles = (f for f in files if f.endswith('.age'))
+            for file in encryptedFiles:
+
+                encryptedFilePath = os.path.join(root, file)
+                relativePath = os.path.relpath(encryptedFilePath, GlobalEnv.ENCRYPTED_PATH)
+                decryptedFilePath = os.path.join(GlobalEnv.DECRYPTED_PATH, relativePath[:-4])  # remove .age
+
+                os.makedirs(os.path.dirname(decryptedFilePath), exist_ok=True)
+
+                errorCode = GlobalEnv.decryptFile(encryptedFilePath, decryptedFilePath, passphrase)
+
+                if errorCode == 0:
+                    continue
+
+                elif errorCode == 1:
+                    print(f'Bad passphrase. Cannot decrypt files', file=sys.stderr)
+                    return
+
+                else:
+                    print(f'[ERROR] Could not dectypt file: {encryptedFilePath}', file=sys.stderr)
+
+                continue
+
+            continue
