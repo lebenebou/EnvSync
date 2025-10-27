6619b4ab35086d7c66defdd6835d9ec3a23ab836
YAMMINE Youssef <yoyammine@murex.com>
2025-10-27 09:11:37 +0200

    CLI: add workingDir argument

--- src/EnvSync/utils/cli.py
+++ src/EnvSync/utils/cli.py
@@ -1,14 +1,19 @@
 
 import subprocess
 import sys
+import os
 
 def runDetached(command: str):
 
     subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
-def runCommand(command: str) -> subprocess.CompletedProcess:
+def runCommand(command: str, workingDir: str = None) -> subprocess.CompletedProcess:
 
-    result = subprocess.run(command, shell=True, text=True, capture_output=True, encoding="latin1")
+    if workingDir is None:
+        workingDir = os.getcwd()
+
+    assert os.path.isdir(workingDir), f'cli: Invalid working directory: {workingDir}'
+    result = subprocess.run(command, shell=True, text=True, capture_output=True, encoding="latin1", cwd=workingDir)
 
     if result.returncode != 0:
         print(f'Error while running command: {command}', file=sys.stderr)
@@ -18,11 +23,11 @@ def runCommand(command: str) -> subprocess.CompletedProcess:
     result.stderr = result.stderr.strip()
     return result
 
-def commandOutput(command: str) -> str: return runCommand(command).stdout
+def commandOutput(command: str, workingDir: str) -> str: return runCommand(command, workingDir = workingDir).stdout
 
 if __name__ == "__main__":
 
-    result = runCommand("echo Hello World!")
+    result = runCommand("echo Hello World!", workingDir=os.getcwd())
 
     print(f"Output: {result.stdout}")
     print(f"Errors: {result.stderr}")
