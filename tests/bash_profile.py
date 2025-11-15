e70e6bdd224dd3f2c328717e56db025049b87497
YAMMINE Youssef <yoyammine@murex.com>
2025-11-15 11:18:43 +0200

    Test: add some tests for bashprofile syntax checking

---
+++ tests/bash_profile.py
@@ -0,0 +1,34 @@
+
+import sys, os
+import time
+
+from EnvSyncTest import EnvSyncTest
+from GlobalEnv import GlobalEnv
+
+from utils import cli
+
+BASH_PROFILE_PY = os.path.join(GlobalEnv().repoSrcPath, 'config', 'BashProfile.py')
+
+def pythonScriptGeneratesCorrectSyntax() -> bool:
+
+    result = cli.runCommand(f'python "{BASH_PROFILE_PY}" | bash -n', workingDir=GlobalEnv().repoRootPath)
+    return result.returncode == 0
+
+def homeBashProfileHasCorrectSyntax() -> bool:
+
+    homeBashProfile: str = os.path.join(GlobalEnv().getBashProfilePath())
+
+    result = cli.runCommand(f'bash -n "{homeBashProfile}"', workingDir=GlobalEnv().repoRootPath)
+    return result.returncode == 0
+
+if __name__ == '__main__':
+
+    tests: list[EnvSyncTest] = [
+        EnvSyncTest('BashProfile.py generates correct bash syntax').asserts(pythonScriptGeneratesCorrectSyntax),
+        EnvSyncTest('Home .bash_profile has correct bash syntax').asserts(homeBashProfileHasCorrectSyntax),
+    ]
+
+    allSuccessful: bool = EnvSyncTest.runTests(tests)
+
+    if not allSuccessful:
+        exit(1)
