33a3dd2054c90ecc310091178f719bfa9fbee0b3
YAMMINE Youssef <yoyammine@murex.com>
2025-12-09 15:48:04 +0200

    Bashprofile: thor doesnt use git

--- src/config/BashProfile.py
+++ src/config/BashProfile.py
@@ -538,7 +538,7 @@ if __name__ == "__main__":
 
     *murexCliOptions(),
     *murexWelcomeMessage(),
-    enableGitUntrackedCacheForMurexVersion(),
+    # enableGitUntrackedCacheForMurexVersion(),
 
     *initSSH(),
 
